7550576d21fa0716f048af5d7056260a0db9cb61
JonTDean <JonathanTomDean@Gmail.Com>
2020-03-14 20:16:40 -0400

    Template laid out, refer to IRL srs sheet for further updates.

---
+++ index.js
@@ -0,0 +1,8 @@
+/* 
+Sites Used:
+https://www.edureka.co/blog/javascript-calculator/
+https://freshman.tech/calculator/
+https://developer.mozilla.org/en-US/docs/Web/API/Node/textContent
+https://www.freecodecamp.org/news/how-to-build-an-html-calculator-app-from-scratch-using-javascript-4454b8714b98/
+
+*/
